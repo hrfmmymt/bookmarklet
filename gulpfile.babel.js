dbfa0822cc5808e877027fa788c67504159e211d
hrfmmymt <hrfmmymt@gmail.com>
2017-09-12 12:40:05 +0900

    babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from "gulp";
 import eslint from "gulp-eslint";
 import bookmarklet from "gulp-bookmarklet";
+import babel from "gulp-babel";
 
 gulp.task("bookmarklet", ["lint"], () => {
   return gulp.src("./src/*.js")
+    .pipe(babel())
     .pipe(bookmarklet())
     .pipe(gulp.dest("./dist/"));
 });
